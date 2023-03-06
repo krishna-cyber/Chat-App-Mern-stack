7adf2c505febee7a16c0613751f616ec8a6c3bee
krishna tiwari <tiwarikrishna54321@gmail.com>
2023-03-06 10:09:01 +0545

    controller created

--- backend/routes/simpleRoute.js
+++ backend/routes/simpleRoute.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const { home } = require("../controllers/controller");
+
+router.get("/", home);
 
 module.exports = router;
